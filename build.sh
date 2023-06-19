eec36a41ee9187da19c38574868d283fca2b39a9
wpp <pengpeng.wang@gemii.cc>
2023-06-19 13:43:36 +0800

    add releases

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/zsh
+export GOARCH=amd64
 
 export GOOS=windows
-export GOARCH=amd64
-go build -ldflags "-s -w"  -o cpss.exe main.go
+go build -ldflags "-s -w"  -o bin/cpss.exe main.go
 
-export GOOS=windows
-export GOARCH=amd64
-go build -ldflags "-s -w"  -o cpss.exe main.go
+export GOOS=linux
+go build -ldflags "-s -w"  -o bin/cpssux main.go
+
+export GOOS=darwin
+go build -ldflags "-s -w"  -o bin/cpss main.go
